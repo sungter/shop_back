aa4cdf27319f36a33e36ffabcaab63b56039fdcb
sungter <eroomeuzz@naver.com>
2025-03-17 17:52:08 +0900

    20250317

--- src/main/java/com/green/book_shop/book/controller/BookController.java
+++ src/main/java/com/green/book_shop/book/controller/BookController.java
@@ -14,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
   private final BookService bookService;
 
+  //도서 등록 api
   @PostMapping("")
   public void regBook(@RequestBody BookDTO bookDTO){
+    //첨부파일(도서 이미지) 업로드
+
+
+    //book 테이블에 데이터 insert
     bookService.regBook(bookDTO);
   }
 }
--- src/main/java/com/green/book_shop/test/TestController.java
+++ src/main/java/com/green/book_shop/test/TestController.java
@@ -1,8 +1,6 @@
 package com.green.book_shop.test;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -13,6 +11,11 @@ public class TestController {
     return 5;
   }
 
+  //첨부파일 연습 1
+  @PostMapping("/upload1")
+  public void upload1(){
+
+  }
 
 
 
